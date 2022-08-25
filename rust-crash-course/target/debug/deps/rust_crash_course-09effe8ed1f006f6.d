34ded1b97bf3fb5ba1feea61e965f105e57c5cad
doug <doug@grafbase.com>
2022-08-25 05:38:51 -0700

    First commit of Rust crash course.

---
+++ rust-crash-course/target/debug/deps/rust_crash_course-09effe8ed1f006f6.d
@@ -0,0 +1,5 @@
+c:\GitHub\DougAtGrafbase\rust-crash-course\rust-crash-course\target\debug\deps\rust_crash_course-09effe8ed1f006f6.rmeta: src\main.rs
+
+c:\GitHub\DougAtGrafbase\rust-crash-course\rust-crash-course\target\debug\deps\rust_crash_course-09effe8ed1f006f6.d: src\main.rs
+
+src\main.rs:
---
+++ rust-crash-course/target/debug/rust-crash-course.d
@@ -0,0 +1,1 @@
+C:\GitHub\DougAtGrafbase\rust-crash-course\rust-crash-course\target\debug\rust-crash-course.exe: C:\GitHub\DougAtGrafbase\rust-crash-course\rust-crash-course\src\main.rs
