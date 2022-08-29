eab6e9e29aebe6bf00b9dfa9b03ffe89c6e801a7
Doug <doug@grafbase.com>
2022-08-29 13:54:41 -0700

    Code for chapter 3 of Rust crash course: https://www.youtube.com/watch?v=bUbO73SI0LI&list=PL6yRaaP0WPkWRsXJgdnw9lj1vchAaKwfS&index=3

--- rust-crash-course/src/main.rs
+++ rust-crash-course/src/main.rs
@@ -1,5 +1,12 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Hello, world!");
+    // Tuples
+    let personal_data = (70, "Doug");
+    let (age, name) = personal_data;
+
+    println!("{} is {} years old.", name, age);
+
+    let first_name = personal_data.1;
+    println!("{} is your first name.", first_name);
 }
